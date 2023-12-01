c46023efc6a16073a8daffc3e07766dff9f90553
DwieTwarze2000 <pawelbinkowski2000@gmail.com>
2023-12-01 23:12:19 +0100

    RR handling bad number

--- frontend/src/components/BadNumberComponent.vue
+++ frontend/src/components/BadNumberComponent.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Bad Number</h1>
+  <div class="d-flex flex-column align-items-center mt-5 mb-5">
+    <div class="text fs-4">Został wybrany nieprawidłowy numer telefonu!</div>
+    <button class="mt-4 btn main-button active" @click="callAgain()">
+      Zadzwoń ponownie
+    </button>
+  </div>
 </template>
+<script setup lang="ts">
+import { useStore } from 'vuex';
+import { CallStatus } from '../types/call.type';
+
+const store = useStore();
+
+const callAgain = (): void => {
+  store.commit('setPhoneCallStatus', CallStatus.START);
+};
+</script>
