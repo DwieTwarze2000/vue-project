61ea54b1c42be8fb0aa8f3547f4097dc91fec732
DwieTwarze2000 <pawelbinkowski2000@gmail.com>
2023-12-01 23:17:46 +0100

    II handling Failed status

--- frontend/src/components/FailedComponent.vue
+++ frontend/src/components/FailedComponent.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Failed</h1>
+  <div class="d-flex flex-column align-items-center mt-5 mb-5">
+    <div class="text fs-4">Niestety, nie udało się nawiązać połączenia!</div>
+    <button class="mt-4 btn main-button active" @click="callAgain()">
+      Zadzwoń ponownie
+    </button>
+  </div>
 </template>
+<script setup lang="ts">
+import { useStore } from 'vuex';
+import { CallStatus } from '../types/call.type';
+
+const store = useStore();
+
+const callAgain = (): void => {
+  store.commit('setPhoneCallStatus', CallStatus.START);
+};
+</script>
