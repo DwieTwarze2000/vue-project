2b80768891bcadc279a08dbb03da7188e620e0c3
DwieTwarze2000 <pawelbinkowski2000@gmail.com>
2023-12-03 00:47:26 +0100

    added option to filter call history based on status

--- frontend/src/views/PhoneCallHistoryView.vue
+++ frontend/src/views/PhoneCallHistoryView.vue
@@ -7,6 +7,38 @@
       </h2>
     </div>
     <div class="mt-4 mx-2 mx-sm-5">
+      <div class="d-flex align-items-center">
+        <button
+          class="btn main-button me-3"
+          :class="{ active: showSuccess }"
+          @click="showSuccess = !showSuccess"
+        >
+          {{ $t('phoneCallHistory.success') }}
+        </button>
+        <button
+          class="btn main-button me-3"
+          :class="{ active: showFailed }"
+          @click="showFailed = !showFailed"
+        >
+          {{ $t('phoneCallHistory.failed') }}
+        </button>
+        <button
+          class="btn main-button me-3"
+          :class="{ active: showBusy }"
+          @click="showBusy = !showBusy"
+        >
+          {{ $t('phoneCallHistory.busy') }}
+        </button>
+        <button
+          class="btn main-button me-3"
+          :class="{ active: showNoAnswer }"
+          @click="showNoAnswer = !showNoAnswer"
+        >
+          {{ $t('phoneCallHistory.noAnswer') }}
+        </button>
+      </div>
+    </div>
+    <div class="mt-4 mx-2 mx-sm-5">
       <table>
         <thead>
           <tr>
@@ -19,40 +51,42 @@
         </thead>
         <tbody>
           <tr v-for="(phoneCallData, idx) in historyData" :key="idx">
-            <td>{{ phoneCallData.phoneNumber }}</td>
-            <td>{{ getDate(phoneCallData.createdAt) }}</td>
-            <td>{{ getTime(phoneCallData.createdAt) }}</td>
-            <td>
-              <span v-if="phoneCallData.status === CallStatus.ANSWERED">
-                {{ formatSeconds(phoneCallData.callDuration) }}
-              </span>
-              <span v-else>-</span>
-            </td>
-            <td
-              v-if="phoneCallData.status === CallStatus.ANSWERED"
-              class="status bg-success"
-            >
-              {{ $t('phoneCallHistory.success') }}
-            </td>
+            <template v-if="showRow(phoneCallData)">
+              <td>{{ phoneCallData.phoneNumber }}</td>
+              <td>{{ getDate(phoneCallData.createdAt) }}</td>
+              <td>{{ getTime(phoneCallData.createdAt) }}</td>
+              <td>
+                <span v-if="phoneCallData.status === CallStatus.ANSWERED">
+                  {{ formatSeconds(phoneCallData.callDuration) }}
+                </span>
+                <span v-else>-</span>
+              </td>
+              <td
+                v-if="phoneCallData.status === CallStatus.ANSWERED"
+                class="status bg-success"
+              >
+                {{ $t('phoneCallHistory.success') }}
+              </td>
 
-            <td
-              v-if="phoneCallData.status === CallStatus.BUSY"
-              class="status bg-warning"
-            >
-              {{ $t('phoneCallHistory.busy') }}
-            </td>
-            <td
-              v-if="phoneCallData.status === CallStatus.NO_ANSWER"
-              class="status bg-warning"
-            >
-              {{ $t('phoneCallHistory.noAnswer') }}
-            </td>
-            <td
-              v-if="phoneCallData.status === CallStatus.FAILED"
-              class="status bg-danger"
-            >
-              {{ $t('phoneCallHistory.failed') }}
-            </td>
+              <td
+                v-if="phoneCallData.status === CallStatus.BUSY"
+                class="status bg-warning"
+              >
+                {{ $t('phoneCallHistory.busy') }}
+              </td>
+              <td
+                v-if="phoneCallData.status === CallStatus.NO_ANSWER"
+                class="status bg-warning"
+              >
+                {{ $t('phoneCallHistory.noAnswer') }}
+              </td>
+              <td
+                v-if="phoneCallData.status === CallStatus.FAILED"
+                class="status bg-danger"
+              >
+                {{ $t('phoneCallHistory.failed') }}
+              </td>
+            </template>
           </tr>
         </tbody>
       </table>
@@ -70,6 +104,26 @@ import { getDate, getTime } from '../utils/date.utils';
 const store = useStore();
 const isLogged = ref(false);
 const historyData = ref([]);
+
+const showSuccess = ref(true);
+const showBusy = ref(true);
+const showNoAnswer = ref(true);
+const showFailed = ref(true);
+
+const showRow = (phoneCallData: PhoneCallData): boolean => {
+  switch (phoneCallData.status) {
+    case CallStatus.ANSWERED:
+      return showSuccess.value;
+    case CallStatus.BUSY:
+      return showBusy.value;
+    case CallStatus.NO_ANSWER:
+      return showNoAnswer.value;
+    case CallStatus.FAILED:
+      return showFailed.value;
+    default:
+      return false;
+  }
+};
 
 const fetchPhoneHistoryData = async (): Promise<void> => {
   const phoneCallHistoryData: PhoneCallData[] = await defaultGet(
